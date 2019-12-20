561b2c18be8b7c30b6454ca93e4205e0e4e368b7
Namhoon Kim <30471027+nhk9680@users.noreply.github.com>
2019-12-21 02:44:01 +0900

    Add files via upload

---
+++ video2jpg.py
@@ -0,0 +1,13 @@
+import cv2
+print(cv2.__version__)
+vidcap = cv2.VideoCapture('Sejong.mp4')
+success,image = vidcap.read()
+print(success)
+count = 0
+#success = True
+while success:
+  cv2.imwrite(("frame%05d.jpg" % count), image)     # save frame as JPEG file
+  success,image = vidcap.read()
+  print('Read a new frame: ', success)
+  count += 1
+  if(cnt): break
